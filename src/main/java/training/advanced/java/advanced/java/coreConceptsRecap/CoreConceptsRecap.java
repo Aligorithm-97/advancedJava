76df422f545f1d43cac02f9df68763ebdebb1770
ali <atemurtas97@gmail.com>
2025-01-02 16:42:04 +0300

    aligorithm-javaCore

---
+++ src/main/java/training/advanced/java/advanced/java/coreConceptsRecap/CoreConceptsRecap.java
@@ -0,0 +1,22 @@
+package training.advanced.java.advanced.java.coreConceptsRecap;
+
+public class CoreConceptsRecap {
+    private String oopRecap;
+    private String abstraction;
+    private String generics;
+    private String collections;
+    private String usingFinal;
+    private String concurrency;
+    private String javaNetworking;
+    private String springBootCore;
+    private String springBootAdvanced;
+    private String otherEssentialParts;
+
+    // Focus specifically on the following concepts :
+        // Interview questions -> Static, autoboxing etc.
+        // Java Data Structures and how to use them efficiently (Hashmap , ArrayList etc.).
+        // Concurrency
+        // Advanced Spring boot -> reflections , custom Annotation etc.
+        // Master java data structures and spring boot logic
+
+}
